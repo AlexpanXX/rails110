23d45b5ef4e67d263d35fe51230772c04135faf6
AlexPan <ppgod@live.cn>
2017-03-30 12:21:11 +0800

    implement movies_controller methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,53 @@
 class MoviesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edti, :update, :destroy]
+  before_action :find_movie_and_check_permission, only: [:edit, :update, :destroy]
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.fins(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.user = current_user
+
+    if @movie.save
+      redirect_to moives_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movies_path, notice: "Movie successfully updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path, alert: "Movie deleted."
+  end
+
+  private
+
+  def find_movie_and_check_permission
+    @movie = Movie.find(params[:id])
+    if @movie.user != current_user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :description)
+  end
 end
